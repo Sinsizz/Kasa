0546b664d7264f7b5ceb51a02687819ae05867cd
Théo Rouillard <theo.rouillard@outlook.fr>
2024-07-08 21:47:49 +0200

    finition de la page 404 et remplacement de l'icone/titre dans l'onglet

--- my-react-app/src/NotFound.jsx
+++ my-react-app/src/NotFound.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
-
+import { Link } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Page non trouvée</h1>
-      <p>Désolé, la page que vous recherchez n'existe pas.</p>
+    <>
+    <Header/>
+    <div className='Error'>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className='lien'>Retourner sur la page d’accueil</Link>
     </div>
+    <Footer/>
+    </>
+    
   );
 }
 
