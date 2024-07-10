60cb33e8c083fcc9b80b39cf95f792d35eb16bbd
Théo Rouillard <theo.rouillard@outlook.fr>
2024-07-10 17:43:13 +0200

    Mise en place de mes scroll bar dans mes pages carddetails

--- my-react-app/src/CardDetails.jsx
+++ my-react-app/src/CardDetails.jsx
@@ -2,21 +2,39 @@
 import { useParams } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import Tag from './Tag';
+import Scrollbar from './Scrollbar.jsx';
 
 function CardDetails({ data }) {
   const { cardId } = useParams();
   const card = data[cardId];
 
+  const formatEquipments = (equipments) => {
+    return equipments.join('\n');
+  };
+
   return (
     <>
       <Header /> 
-    <section className='carddetails'>
+      <section className='carddetails'>
         <img src={card.cover} alt={card.title} />
         <div>
-        <h2>{card.title}</h2>
-        <p>{card.location}</p>
+          <h2>{card.title}</h2>
+          <p>{card.location}</p>
+          <Tag tags={card.tags} />
         </div>
-    </section>
+        <div className="scrollbar-container">
+        <Scrollbar 
+          title="Description"
+          content={card.description}
+        />
+        <Scrollbar 
+          title="Équipements"
+          content={formatEquipments(card.equipments)}
+        />
+      </div>
+      </section>
+      
       <Footer /> 
     </>
   );
