242da3f15f78c5a4842128a03b688a87a1ea8b90
Théo Rouillard <theo.rouillard@outlook.fr>
2024-07-11 15:46:51 +0200

    creation du composant rating

---
+++ my-react-app/src/Rating.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import starFilled from '../assets/star-filled.png'; // Assurez-vous d'avoir cette image
+import starEmpty from '../assets/star-empty.png';  // Assurez-vous d'avoir cette image
+
+function Rating({ rating }) {
+  const totalStars = 5;
+  const filledStars = parseInt(rating);
+
+  return (
+    <div className="rating">
+      {[...Array(totalStars)].map((_, index) => (
+        <img
+          key={index}
+          src={index < filledStars ? starFilled : starEmpty}
+          alt={index < filledStars ? "Étoile remplie" : "Étoile vide"}
+          className="star"
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Rating;
