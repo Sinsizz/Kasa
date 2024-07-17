af095bdf86aceead8e776d3975b65405988f5668
Théo Rouillard <theo.rouillard@outlook.fr>
2024-07-17 16:23:16 +0200

    Finis

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,18 +2,19 @@ import data from "./data.json";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './pages/App.jsx';
+import Home from './pages/Home.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import CardDetails from './pages/CardDetails.jsx';
 import NotFound from './pages/NotFound';
 import './index.css';
+import Home from "./pages/Home.jsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route exact path="/" element={<App />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/card/:cardId" element={<CardDetails data={data} />} />
         <Route path="*" element={<NotFound />} />
