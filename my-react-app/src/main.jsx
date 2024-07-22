533c4481c313ea8f8588269f797c97131dfd019c
Théo Rouillard <theo.rouillard@outlook.fr>
2024-07-22 16:40:34 +0200

    Commentaire

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -8,14 +8,20 @@ import NotFound from './pages/NotFound';
 import './index.css';
 import Home from "./pages/Home.jsx";
 
-
+// Rendu de l'application React
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Utilisation du routeur React Router */}
     <Router>
+      {/* Définition des routes */}
       <Routes>
+        {/* Route pour la page d'accueil */}
         <Route exact path="/" element={<Home />} />
+        {/* Route pour la page "À propos" */}
         <Route path="/about" element={<AboutPage />} />
+        {/* Route pour les détails d'une carte, avec un paramètre d'URL pour l'ID de la carte */}
         <Route path="/card/:cardId" element={<CardDetails data={data} />} />
+        {/* Route pour gérer les URLs non reconnues (404) */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
